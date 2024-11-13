fccbc335001fa795346ea5db20a33e6dabf6f768
Muluken <mulukeneshetu70@gmail.com>
2024-11-13 17:30:15 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,45 +1,52 @@
 #!/usr/bin/python3
+"""
+UTF-8 Encoding Validation
+"""
+
+
 def validUTF8(data):
     """
-    Determines if a given data set represents a valid UTF-8 encoding.
-
-    Arguments:
-    data -- A list of integers representing bytes of data.
-
-    Returns:
-    True if the data is a valid UTF-8 encoding, else False.
+    Implementation
     """
-    i = 0
-    n = len(data)
-
-    while i < n:
-        byte = data[i]
-
-        # Check for 1-byte character (0xxxxxxx)
-        if byte <= 0x7F:
-            i += 1
+    point = 0
+    while point < len(data):
+        Byte1 = data[point] & 255
+        if not Byte1 >> 7:
+            point += 1
             continue
-
-        # Check for 2-byte character (110xxxxx 10xxxxxx)
-        elif byte >= 0xC0 and byte <= 0xDF:
-            if i + 1 >= n or not (0x80 <= data[i + 1] <= 0xBF):
+        elif (Byte1 >> 5) == 6:
+            try:
+                Byte2 = data[point + 1]
+                if (Byte2 >> 6) == 2:
+                    point += 2
+                    continue
+                else:
+                    return False
+            except IndexError:
                 return False
-            i += 2
-
-        # Check for 3-byte character (1110xxxx 10xxxxxx 10xxxxxx)
-        elif byte >= 0xE0 and byte <= 0xEF:
-            if i + 2 >= n or not (0x80 <= data[i + 1] <= 0xBF) or not (0x80 <= data[i + 2] <= 0xBF):
+        elif (Byte1 >> 4) == 14:
+            try:
+                Byte2 = data[point + 1]
+                Byte3 = data[point + 2]
+                if (Byte2 >> 6) == (Byte3 >> 6) == 2:
+                    point += 3
+                    continue
+                else:
+                    return False
+            except IndexError:
                 return False
-            i += 3
-
-        # Check for 4-byte character (11110xxx 10xxxxxx 10xxxxxx 10xxxxxx)
-        elif byte >= 0xF0 and byte <= 0xF7:
-            if i + 3 >= n or not (0x80 <= data[i + 1] <= 0xBF) or not (0x80 <= data[i + 2] <= 0xBF) or not (0x80 <= data[i + 3] <= 0xBF):
+        elif (Byte1 >> 3) == 30:
+            try:
+                Byte2 = data[point + 1]
+                Byte3 = data[point + 2]
+                Byte4 = data[point + 3]
+                if (Byte2 >> 6) == (Byte3 >> 6) == (Byte4 >> 6) == 2:
+                    point += 4
+                    continue
+                else:
+                    return False
+            except IndexError:
                 return False
-            i += 4
-
-        # If the byte doesn't match any of the above ranges, return False
         else:
             return False
-
     return True
