95b37f8b7c965f68afb757dce8103fa033fe2f61
Muluken <mulukeneshetu70@gmail.com>
2024-11-17 17:11:35 +0300

    Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,38 +1,30 @@
 #!/usr/bin/node
-// Prints all characters of a Star Wars movie using the ALX Star Wars API
 
-if (process.argv.length !== 3) {
-  process.exit();
-}
-const movieID = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 const request = require('request');
+const ID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${ID}/`;
 
-// Recursive function to get and print character names from a list of URLs
-function printNextCharacter (urls) {
-  // Remove first URL from list and parse it
-  const characterURL = urls.shift();
-  if (characterURL) {
-    request(characterURL, function (error, response, body) {
-      if (!error) {
-        // Parse and print the character name from the response body
-        console.log(JSON.parse(body).name);
-        // Move to next character in the list
-        printNextCharacter(urls);
-      }
-    });
+function getCharacter (listChar, index) {
+  if (index === listChar.length) {
+    return;
   }
-}
-
-// Fetches a list of character's URLs then uses a callback to print each one's name
-function printStarWarsCharacters (url) {
-  // Get list of URLs for characters in selected film
-  request(url, function (error, response, body) {
-    if (!error) {
-      // Call recursive function to fetch and print character names from URLs list
-      printNextCharacter(JSON.parse(body).characters);
+  request(`${listChar[index]}`, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const character = JSON.parse(response.body);
+      console.log(character.name);
+      getCharacter(listChar, index + 1);
     }
   });
 }
 
-printStarWarsCharacters(url);
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const film = JSON.parse(response.body);
+    const listChar = film.characters;
+    getCharacter(listChar, 0);
+  }
+});
