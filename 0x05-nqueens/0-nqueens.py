1a0d6261cff158a272388e07e3914159b7fe4366
Muluken <mulukeneshetu70@gmail.com>
2024-11-26 14:58:54 +0300

    N Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,47 +1,82 @@
 #!/usr/bin/python3
 """
-The N queens puzzle is the challenge of placing
-N non-attacking queens on an NxN chessboard
+N Queens Problem Solver
 """
+
 import sys
 
 
-from sys import argv, exit
+def print_usage_and_exit():
+    """Print usage and exit with status 1"""
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+
+def print_number_error_and_exit():
+    """Print number error and exit with status 1"""
+    print("N must be a number")
+    sys.exit(1)
+
+
+def print_minimum_error_and_exit():
+    """Print minimum error and exit with status 1"""
+    print("N must be at least 4")
+    sys.exit(1)
+
+
+def is_safe(board, row, col, n):
+    """
+    Check if it's safe to place a queen at board[row][col]
+    """
+    # Check this column
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+
+def solve_nqueens(n):
+    """
+    Solve the N Queens problem and return all solutions
+    """
+    def backtrack(row):
+        """Recursive backtracking"""
+        if row == n:
+            solutions.append([[i, board[i]] for i in range(n)])
+            return
+        for col in range(n):
+            if is_safe(board, row, col, n):
+                board[row] = col
+                backtrack(row + 1)
+                board[row] = -1
+
+    board = [-1] * n  # Initialize board with -1
+    solutions = []
+    backtrack(0)
+    return solutions
+
+
+def main():
+    """
+    Main function to parse input and print solutions
+    """
+    if len(sys.argv) != 2:
+        print_usage_and_exit()
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print_number_error_and_exit()
+
+    if n < 4:
+        print_minimum_error_and_exit()
+
+    solutions = solve_nqueens(n)
+    for solution in solutions:
+        print(solution)
+
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print('Usage: nqueens N')
-        exit(1)
-    try:
-        n = int(argv[1])
-    except BaseException:
-        print('N must be a number')
-        exit(1)
-    if n < 4:
-        print('N must be at least 4')
-        exit(1)
-
-    solution = []
-
-    def solve_queens(row, n, solution):
-        if (row == n):
-            print(solution)
-        else:
-            for col in range(n):
-                placement = [row, col]
-                if valid_placement(solution, placement):
-                    solution.append(placement)
-                    solve_queens(row + 1, n, solution)
-                    solution.remove(placement)
-
-    def valid_placement(solution, placement):
-        for queen in solution:
-            if queen[1] == placement[1]:
-                return False
-            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
-                return False
-            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
-                return False
-        return True
-
-    solve_queens(0, n, solution)
+    main()
