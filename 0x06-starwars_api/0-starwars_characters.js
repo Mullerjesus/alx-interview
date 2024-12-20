7d9c2065c6cacdc23cee77c62332e98545b6f07f
Muluken <mulukeneshetu70@gmail.com>
2024-12-20 10:37:30 +0300

    Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,63 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-// Get the movie ID from the command-line argument
 const movieId = process.argv[2];
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
+  method: 'GET'
+};
 
-// API endpoint for the movie
-const url = `https://swapi.dev/api/films/${movieId}/`;
-
-// Make a request to fetch movie details
-request(url, (err, res, body) => {
-  if (err) {
-    console.error(err);
-    return;
+request(options, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-
-  if (res.statusCode !== 200) {
-    console.error(`Error: Failed to fetch data (status code: ${res.statusCode})`);
-    return;
-  }
-
-  // Parse the response body
-  const data = JSON.parse(body);
-
-  // Check if characters exist
-  if (!data.characters || data.characters.length === 0) {
-    console.log('No characters found for this movie.');
-    return;
-  }
-
-  // Fetch each character's name in order
-  const characters = data.characters;
-  const fetchCharacterNames = (index) => {
-    if (index >= characters.length) {
-      return; // Done
-    }
-
-    // Fetch individual character data
-    request(characters[index], (err, res, body) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-
-      if (res.statusCode === 200) {
-        const characterData = JSON.parse(body);
-        console.log(characterData.name);
-      } else {
-        console.error(`Failed to fetch character (status code: ${res.statusCode})`);
-      }
-
-      // Recursively fetch the next character
-      fetchCharacterNames(index + 1);
-    });
-  };
-
-  fetchCharacterNames(0); // Start fetching
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
